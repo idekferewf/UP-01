ee8429b8f924cd9f7b4e74130c20b1ae2bda6c0e
st56 <alemehov1@gmail.com>
2025-09-26 09:26:54 +0300

    создан проект & добавлен класс Product

---
+++ AptekaEU/AptekaEULibrary/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace AptekaEULibrary
+{
+    public class Product
+    {
+        [DisplayName("ID товара")]
+        public int Id { get; set; }
+
+        [DisplayName("Наименование")]
+        public string Name { get; set; }
+
+        [DisplayName("ID категории")]
+        public int CategoryId { get; set; }
+
+        [DisplayName("Цена продажи")]
+        public decimal PurchasePrice { get; set; }
+
+        [DisplayName("Цена продажи")]
+        public decimal SalePrice { get; set; }
+
+        [DisplayName("Актуальное количество")]
+        public int ActualQuantity { get; set; }
+    }
+}
---
+++ AptekaEU/AptekaEULibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AptekaEULibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AptekaEULibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("82d7d5f3-d3ba-4292-b37b-3d7ceb9fcd16")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
