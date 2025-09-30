e91bb470f8b311cf94a7e77e259bc1bbc8accaee
студент 403 <st403@tpk.local>
2025-09-30 12:59:48 +0300

    добавлен класс Product

---
+++ AptekaEu/AptekaEuLib/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace AptekaEuLib
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public int ActualQuantity {  get; set; }
+    }
+}
