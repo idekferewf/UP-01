0a9ac8d5d5f099f801a4718373bea843b19a719a
sellasouls666 <pgbc303@gmail.com>
2025-10-01 10:09:35 +0300

    добавлено множество тестов черех DataRow

--- AptekaEu/AptekaEuTesting/TProductService.cs
+++ AptekaEu/AptekaEuTesting/TProductService.cs
@@ -8,26 +8,38 @@ namespace AptekaEuTesting
     public class TProductService
     {
         [TestMethod]
-        public void TAddProduct_ValidData()
+        [DataRow("Парацетамол 500 мг", 1, 299.00, 399.00, 100, "")]
+        [DataRow("", 2, 249.00, 319.00, 22, "Наименование товара не может быть пустым.")]
+        [DataRow("Парацетамол 250 мг", 6, 229.00, -129.00, 64, "Цена продажи не может быть отрицательной.")]
+        [DataRow("Парацетамол 900 мг", 3, -189.00, 399.00, 8, "Цена закупки не может быть отрицательной.")]
+        public void TAddProduct(string name, int categoryId, double purchasePrice, double salePrice, int actualQuantity, string expected)
         {
             var mockRepo = new Mock<IProductsRepository>();
 
             var product = new Product()
             {
-                Name = "Парацетамол 500 мг",
-                CategoryId = 1,
-                PurchasePrice = 299.00M,
-                SalePrice = 399.00M,
-                ActualQuantity = 100
+                Name = name,
+                CategoryId = categoryId,
+                PurchasePrice = purchasePrice,
+                SalePrice = salePrice,
+                ActualQuantity = actualQuantity
             };
+
+            mockRepo.Setup(r => r.AddProduct(product));
 
             var productService = new ProductService(mockRepo.Object);
 
             string error = productService.AddProduct(product);
 
-            Assert.AreEqual(string.Empty, error);
+            Assert.AreEqual(expected, error);
 
-            mockRepo.Verify(r => r.AddProduct(product), Times.Once);
+            if (string.IsNullOrEmpty(error))
+            {
+                mockRepo.Verify(r => r.AddProduct(product), Times.Once);
+            } else
+            {
+                mockRepo.Verify(r => r.AddProduct(product), Times.Never);
+            }
         }
     }
 }
