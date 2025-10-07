ad175363fee672cde974fc3f2c55ad591de49c49
idekferewf <st50@WS50-09>
2025-10-07 08:19:34 +0300

    добавлен Returns в тесты с mock

--- AptekaEu/AptekaEuTesting/TProductService.cs
+++ AptekaEu/AptekaEuTesting/TProductService.cs
@@ -8,11 +8,11 @@ namespace AptekaEuTesting
     public class TProductService
     {
         [TestMethod]
-        [DataRow("Парацетамол 500 мг", 1, 299.00, 399.00, 100, "")]
-        [DataRow("", 2, 249.00, 319.00, 22, "Наименование товара не может быть пустым.")]
-        [DataRow("Парацетамол 250 мг", 6, 229.00, -129.00, 64, "Цена продажи не может быть отрицательной.")]
-        [DataRow("Парацетамол 900 мг", 3, -189.00, 399.00, 8, "Цена закупки не может быть отрицательной.")]
-        public void TAddProduct(string name, int categoryId, double purchasePrice, double salePrice, int actualQuantity, string expected)
+        [DataRow("Парацетамол 500 мг", 1, 299.00, 399.00, 100, true, "")]
+        [DataRow("", 2, 249.00, 319.00, 22, false, "Наименование товара не может быть пустым.")]
+        [DataRow("Парацетамол 250 мг", 6, 229.00, -129.00, 64, false, "Цена продажи не может быть отрицательной.")]
+        [DataRow("Парацетамол 900 мг", 3, -189.00, 399.00, 8, false, "Цена закупки не может быть отрицательной.")]
+        public void TAddProduct(string name, int categoryId, double purchasePrice, double salePrice, int actualQuantity, bool result, string expected)
         {
             var mockRepo = new Mock<IProductsRepository>();
 
@@ -25,7 +25,7 @@ namespace AptekaEuTesting
                 ActualQuantity = actualQuantity
             };
 
-            mockRepo.Setup(r => r.AddProduct(product));
+            mockRepo.Setup(r => r.AddProduct(product)).Returns(result);
 
             var productService = new ProductService(mockRepo.Object);
 
