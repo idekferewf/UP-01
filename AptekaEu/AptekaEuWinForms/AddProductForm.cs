832122df4d140e546c7cd1d1baa96faea6770f14
st56 <alemehov1@gmail.com>
2025-10-14 10:52:54 +0300

    убрано обнуление полей при отмене создания товара

--- AptekaEu/AptekaEuWinForms/AddProductForm.cs
+++ AptekaEu/AptekaEuWinForms/AddProductForm.cs
@@ -15,11 +15,6 @@ namespace AptekaEuWinForms
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
-            nameTextBox.Text = "";
-            categoryIdNumericUpDown.Value = 0;
-            purchasePriceNumericUpDown.Value = 0;
-            salePriceNumericUpDown.Value = 0;
-            actualQuantiryNumericUpDown.Value = 0;
             Close();
         }
 
