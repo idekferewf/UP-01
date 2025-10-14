bd089c52edf0b715bb0b5bf5bceca229a0af2609
st56 <alemehov1@gmail.com>
2025-10-14 09:54:45 +0300

    добавлен пакет MySql.Data && добавлен класс MySqlProductsReader

---
+++ AptekaEu/AptekaEuLib/MySQLProductsReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AptekaEuLib
+{
+    public class MySQLProductsReader : IProductsRepository
+    {
+        private string myConnectionString = "server=127.0.0.1;uid=root;pwd=vertrigo;database=aptekaeu;";
+
+        public bool AddProduct(Product product)
+        {
+            return true;
+        }
+    }
+}
