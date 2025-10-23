28c84799661624feef69d3f502d48edbe08bd857
st56 <alemehov1@gmail.com>
2025-10-23 13:09:08 +0300

    добавлены тесты для метода удаления товара

--- AptekaEu/AptekaEuTesting/TProductService.cs
+++ AptekaEu/AptekaEuTesting/TProductService.cs
@@ -2,6 +2,8 @@
 using AptekaEuLib.products;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AptekaEuTesting
 {
@@ -43,6 +45,43 @@ namespace AptekaEuTesting
             } else
             {
                 mockRepo.Verify(r => r.AddProduct(product), Times.Never);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 2 }, true, "")]
+        [DataRow(new int[] { 2, 4 }, true, "")]
+        [DataRow(new int[] { 19 }, false, "Данных товаров не существует.")]
+        [DataRow(new int[] { 19, 9 }, false, "Данных товаров не существует.")]
+        public void TDeleteProduct(int[] productIds, bool result, string expected)
+        {
+            var mockRepo = new Mock<IProductsRepository>();
+
+            var existingProducts = new List<Product>
+            {
+                new Product(1) { Name = "Парацетамол 500 мг", Category = new Category(1), PurchasePrice = 299.00, SalePrice = 399.00, ActualQuantity = 100 },
+                new Product(2) { Name = "Парацетамол 250 мг", Category = new Category(1), PurchasePrice = 159.00, SalePrice = 279.00, ActualQuantity = 13 },
+                new Product(3) { Name = "Амоксиклав 625мг таб. №14", Category = new Category(2), PurchasePrice = 199.00, SalePrice = 349.00, ActualQuantity = 79 },
+                new Product(4) { Name = "Мыло жидкое антибактериальное", Category = new Category(3), PurchasePrice = 249.00, SalePrice = 419.00, ActualQuantity = 54 }
+            };
+
+            List<int> productsIdsList = productIds.ToList();
+
+            mockRepo.Setup(r => r.DeleteProducts(productsIdsList)).Returns(result);
+
+            var productService = new ProductService(mockRepo.Object);
+
+            string error = productService.DeleteProducts(productsIdsList);
+
+            Assert.AreEqual(expected, error);
+
+            if (string.IsNullOrEmpty(error))
+            {
+                mockRepo.Verify(r => r.DeleteProducts(productsIdsList), Times.Once);
+            }
+            else
+            {
+                mockRepo.Verify(r => r.DeleteProducts(productsIdsList), Times.Never);
             }
         }
     }
