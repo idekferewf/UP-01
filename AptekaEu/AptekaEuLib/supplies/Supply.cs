a1cabe1707ecc2a5bbac9841a177570c3a9c22a6
st56 <skadeshopp@gmail.com>
2025-11-07 10:01:31 +0300

    добавлен класс поставок (Supply)

---
+++ AptekaEu/AptekaEuLib/supplies/Supply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AptekaEuLib.supplies
+{
+    public class Supply
+    {
+        private int id_;
+
+        public int Id { get { return id_; } }
+
+        public Product Product { get; set; }
+
+        public int SupplierTin { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+        
+        public DateTime ExpiryDate { get; set; }
+
+        public Supply(int id)
+        {
+            id_ = id;
+        }
+    }
+}
