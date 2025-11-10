3fb87cdcc97892c0485c40752f9c1d4117dcd154
idekferewf <skadeshopp@gmail.com>
2025-11-10 18:58:01 +0300

    добавлен класс позиций поставки (SupplyItem)

---
+++ AptekaEu/AptekaEuLib/supplies/SupplyItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AptekaEuLib.supplies
+{
+    public class SupplyItem
+    {
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+    }
+}
