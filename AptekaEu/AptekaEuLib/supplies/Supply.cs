3defc8baf1c16d22a9c4569f677d697a16812b84
st56 <skadeshopp@gmail.com>
2025-11-11 10:12:09 +0300

    переопределён метод Equals для Supply

--- AptekaEu/AptekaEuLib/supplies/Supply.cs
+++ AptekaEu/AptekaEuLib/supplies/Supply.cs
@@ -21,5 +21,52 @@ namespace AptekaEuLib.supplies
         {
             id_ = id;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Supply other = (Supply)obj;
+
+            if (Id != other.Id || SerialNumber != other.SerialNumber || SupplierTin != other.SupplierTin || DeliveryDate != other.DeliveryDate)
+            {
+                return false;
+            }
+
+            if (Items == null && other.Items == null)
+            {
+                return true;
+            }
+            else if (Items == null || other.Items == null)
+            {
+                return false;
+            }
+            else if (Items.Count != other.Items.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                var item1 = Items[i];
+                var item2 = other.Items[i];
+
+                if (
+                    item1.Quantity != item2.Quantity ||
+                    item1.UnitPrice != item2.UnitPrice ||
+                    item1.ProductionDate != item2.ProductionDate ||
+                    item1.ExpiryDate != item2.ExpiryDate ||
+                    item1.Product?.Id != item2.Product?.Id
+                )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
