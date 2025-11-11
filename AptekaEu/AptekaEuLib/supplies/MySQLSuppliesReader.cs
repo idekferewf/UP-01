68b5bdaebbdf3019a8269f3385a7e38999977b81
idekferewf <skadeshopp@gmail.com>
2025-11-11 12:58:05 +0300

    добавлен класс MySQLSuppliesReader

---
+++ AptekaEu/AptekaEuLib/supplies/MySQLSuppliesReader.cs
@@ -0,0 +1,12 @@
+﻿using AptekaEuLib.products;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AptekaEuLib.supplies
+{
+    public class MySQLSuppliesReader
+    {
+    }
+}
