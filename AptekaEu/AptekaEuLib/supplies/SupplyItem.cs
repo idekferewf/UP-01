6c751ce55257769ab52249487467a35430f0ae9a
idekferewf <skadeshopp@gmail.com>
2025-11-12 10:44:08 +0300

    добавлено свойство для отображения стоимости товара && добавлены DisplayName для свойств SupplyItem

--- AptekaEu/AptekaEuLib/supplies/SupplyItem.cs
+++ AptekaEu/AptekaEuLib/supplies/SupplyItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace AptekaEuLib.supplies
 {
@@ -6,12 +7,25 @@ namespace AptekaEuLib.supplies
     {
         public Product Product { get; set; }
 
+        [Browsable(false)]
         public double UnitPrice { get; set; }
 
+        [DisplayName("Стоимость товара")]
+        public string UnitPriceDisplay 
+        {
+            get
+            {
+                return $"{UnitPrice:N2} руб.";
+            }
+        }
+
+        [DisplayName("Количество")]
         public int Quantity { get; set; }
 
+        [DisplayName("Дата производства")]
         public DateTime ProductionDate { get; set; }
 
+        [DisplayName("Срок годности")]
         public DateTime ExpiryDate { get; set; }
     }
 }
