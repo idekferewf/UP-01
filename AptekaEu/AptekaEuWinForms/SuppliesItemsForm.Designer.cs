4c67fad1d133c2f55b0cc5f11773d6882182ee25
idekferewf <skadeshopp@gmail.com>
2025-11-13 22:00:02 +0300

    изменён начальный размер формы && изменён StartPosition

--- AptekaEu/AptekaEuWinForms/SuppliesItemsForm.Designer.cs
+++ AptekaEu/AptekaEuWinForms/SuppliesItemsForm.Designer.cs
@@ -37,21 +37,26 @@
             this.suppliesItemsGridView.AllowUserToAddRows = false;
             this.suppliesItemsGridView.AllowUserToDeleteRows = false;
             this.suppliesItemsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.suppliesItemsGridView.ColumnHeadersHeight = 40;
             this.suppliesItemsGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.suppliesItemsGridView.Location = new System.Drawing.Point(0, 0);
+            this.suppliesItemsGridView.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.suppliesItemsGridView.Name = "suppliesItemsGridView";
             this.suppliesItemsGridView.ReadOnly = true;
+            this.suppliesItemsGridView.RowHeadersWidth = 72;
             this.suppliesItemsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.suppliesItemsGridView.Size = new System.Drawing.Size(800, 450);
+            this.suppliesItemsGridView.Size = new System.Drawing.Size(1289, 702);
             this.suppliesItemsGridView.TabIndex = 1;
             // 
-            // SuppliesForm
+            // SuppliesItemsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1289, 702);
             this.Controls.Add(this.suppliesItemsGridView);
-            this.Name = "SuppliesForm";
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "SuppliesItemsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Позиции поставки – AptekaEu";
             ((System.ComponentModel.ISupportInitialize)(this.suppliesItemsGridView)).EndInit();
             this.ResumeLayout(false);
