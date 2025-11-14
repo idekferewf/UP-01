ee1921661c264f484795bc072ad77299f9d1f0a7
st56 <skadeshopp@gmail.com>
2025-11-14 10:24:50 +0300

    добавлен метод FilterBySuppliesTin для поставок фильтрации по поставщику

--- AptekaEu/AptekaEuLib/supplies/SupplyService.cs
+++ AptekaEu/AptekaEuLib/supplies/SupplyService.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Linq;
 
 namespace AptekaEuLib.supplies
 {
     public class SupplyService
     {
         private BindingList<Supply> supplies_;
+        private BindingList<Supply> filteredSupplies_;
         private ISuppliesRepository suppliesRepository_;
 
         public SupplyService(ISuppliesRepository suppliesRepository)
@@ -16,6 +18,18 @@ namespace AptekaEuLib.supplies
         {
             supplies_ = new BindingList<Supply>(suppliesRepository_.ReadSupplies());
             return supplies_;
+        }
+
+        public BindingList<Supply> FilterBySuppliesTin(string supplierTin)
+        {
+            if (string.IsNullOrEmpty(supplierTin))
+            {
+                filteredSupplies_ = supplies_;
+                return filteredSupplies_;
+            }
+
+            filteredSupplies_ = new BindingList<Supply>(supplies_.Where(s => s.SupplierTin == supplierTin).ToList());
+            return filteredSupplies_;
         }
     }
 }
