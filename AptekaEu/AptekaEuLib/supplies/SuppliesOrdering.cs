7ccc4aeb5c5112ef02bfb4fabf970413a8899ae5
st56 <skadeshopp@gmail.com>
2025-11-14 09:55:28 +0300

    добавлен метод SortBy для сортировки поставок

--- AptekaEu/AptekaEuLib/supplies/SuppliesOrdering.cs
+++ AptekaEu/AptekaEuLib/supplies/SuppliesOrdering.cs
@@ -19,5 +19,36 @@ namespace AptekaEuLib.supplies
         }
 
         public BindingList<Supply> SortedSupplies => sortedSupplies_;
+
+        public void SortBy(string propertyName)
+        {
+            if (sortedSupplies_ == null || !sortedSupplies_.Any())
+            {
+                return;
+            }
+
+            if (currentSortProperty_ == propertyName)
+            {
+                isAscending_ = !isAscending_;
+            }
+            else
+            {
+                currentSortProperty_ = propertyName;
+            }
+
+            PropertyInfo propertyInfo = typeof(Supply).GetProperty(currentSortProperty_);
+
+            List<Supply> supplies;
+            if (isAscending_)
+            {
+                supplies = sortedSupplies_.OrderBy(s => propertyInfo.GetValue(s)).ToList();
+            }
+            else
+            {
+                supplies = sortedSupplies_.OrderByDescending(s => propertyInfo.GetValue(s)).ToList();
+            }
+
+            sortedSupplies_ = new BindingList<Supply>(supplies);
+        }
     }
 }
