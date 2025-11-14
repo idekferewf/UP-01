65eaff069f048fea249fde9ac6ae15faf374228c
st56 <skadeshopp@gmail.com>
2025-11-14 09:53:33 +0300

    добавлена инициализация значений и параметров сортировки по умолчанию в SuppliesOrdering

--- AptekaEu/AptekaEuLib/supplies/SuppliesOrdering.cs
+++ AptekaEu/AptekaEuLib/supplies/SuppliesOrdering.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace AptekaEuLib.supplies
 {
     public class SuppliesOrdering
     {
+        private BindingList<Supply> sortedSupplies_;
+        private string currentSortProperty_;
+        private bool isAscending_;
 
+        public SuppliesOrdering(BindingList<Supply> sortedSupplies)
+        {
+            sortedSupplies_ = sortedSupplies;
+            currentSortProperty_ = "DeliveryDate";
+            isAscending_ = false;
+        }
+
+        public BindingList<Supply> SortedSupplies => sortedSupplies_;
     }
 }
