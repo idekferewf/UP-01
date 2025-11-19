79774c6c4b78f25febd4db4eec81d384d0b89bd5
idekferewf <skadeshopp@gmail.com>
2025-11-19 13:37:12 +0300

    добавлен класс поставщика (Supplier)

---
+++ AptekaEu/AptekaEuLib/supplies/Supplier.cs
@@ -0,0 +1,22 @@
+﻿namespace AptekaEuLib.supplies
+{
+    public class Supplier
+    {
+        private string tin_;
+
+        public string Tin { get {  return tin_; } }
+
+        public string Name;
+
+        public string ContactPerson;
+
+        public string Phone;
+
+        public string Address;
+
+        public Supplier(string tin)
+        {
+            tin_ = tin;
+        }
+    }
+}
