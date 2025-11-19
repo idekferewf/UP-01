9e014364ffda80d1e2ba5b433eac6010f0bbf397
idekferewf <skadeshopp@gmail.com>
2025-11-19 13:50:54 +0300

    реализовано получение и создание поставщиков для поставок

--- AptekaEu/AptekaEuLib/supplies/MySQLSuppliesReader.cs
+++ AptekaEu/AptekaEuLib/supplies/MySQLSuppliesReader.cs
@@ -20,9 +20,11 @@ namespace AptekaEuLib.supplies
                     string query =
                         @"SELECT 
                             supplies.serial_number, supplies.supplier_tin, supplies.delivery_date,
+                            suppliers.name as supplier_name, suppliers.contact_person, suppliers.phone, suppliers.address,
                             supply_items.product_id, supply_items.quantity, supply_items.unit_price, supply_items.production_date, supply_items.expiry_date,
                             products.name as product_name, products.category_id, categories.name as category_name, products.purchase_price, products.sale_price, products.actual_quantity
                         FROM supplies supplies
+                            INNER JOIN suppliers suppliers ON supplies.supplier_tin = suppliers.tin
                             INNER JOIN supply_items supply_items ON supplies.serial_number = supply_items.supply_serial_number
                             INNER JOIN products products ON supply_items.product_id = products.id
                             INNER JOIN categories categories ON products.category_id = categories.id
@@ -41,9 +43,18 @@ namespace AptekaEuLib.supplies
                             /// Создаём объект Supply, если это новая поставка
                             if (currentSupply?.SerialNumber != serialNumber)
                             {
+                                Supplier supplier = new Supplier(reader.GetString("supplier_tin"))
+                                {
+                                    Name = reader.GetString("supplier_name"),
+                                    ContactPerson = reader.GetString("contact_person"),
+                                    Phone = reader.GetString("phone"),
+                                    Address = reader.GetString("address")
+                                };
+
                                 currentSupply = new Supply(serialNumber)
                                 {
                                     SupplierTin = reader.GetString("supplier_tin"),
+                                    Supplier = supplier,
                                     DeliveryDate = reader.GetDateTime("delivery_date"),
                                     Items = new List<SupplyItem>()
                                 };
