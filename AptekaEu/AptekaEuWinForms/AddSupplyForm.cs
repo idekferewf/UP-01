951996e4f08bf73d9cb1981163e2a18762561e0e
st56 <skadeshopp@gmail.com>
2025-11-21 10:58:01 +0300

    добавлено свойство Supply для хранения созданной поставки

--- AptekaEu/AptekaEuWinForms/AddSupplyForm.cs
+++ AptekaEu/AptekaEuWinForms/AddSupplyForm.cs
@@ -1,18 +1,13 @@
 ﻿using AptekaEuLib.supplies;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AptekaEuWinForms
 {
     public partial class AddSupplyForm: Form
     {
+        public Supply Supply { get; set; }
+
         public AddSupplyForm(List<Supplier> suppliers)
         {
             InitializeComponent();
