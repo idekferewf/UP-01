feb82069c23f7ec418f41ad1d62f40e61c19f9cc
idekferewf <skadeshopp@gmail.com>
2025-11-24 09:50:32 +0300

    добавлен метод FillSupplyItems ля заполнения позиций товаров && добавлен обработчик кнопки с проверками (без создания поставки)

--- AptekaEu/AptekaEuWinForms/AddSupplyForm.cs
+++ AptekaEu/AptekaEuWinForms/AddSupplyForm.cs
@@ -9,6 +9,8 @@ namespace AptekaEuWinForms
 {
     public partial class AddSupplyForm: Form
     {
+        private BindingList<SupplyItem> supplyItems_ = new BindingList<SupplyItem>();
+
         public Supply Supply { get; set; }
 
         public AddSupplyForm(List<Supplier> suppliers, BindingList<Product> products)
@@ -17,6 +19,7 @@ namespace AptekaEuWinForms
 
             FillSuppliers(suppliers);
             FillProducts(products);
+            FillSupplyItems();
         }
 
         public void FillSuppliers(List<Supplier> suppliers)
@@ -30,9 +33,41 @@ namespace AptekaEuWinForms
             productsListBox.DataSource = products;
         }
 
+        public void FillSupplyItems()
+        {
+            itemsGridView.DataSource = supplyItems_;
+            itemsGridView.Columns["Category"].Visible = false;
+        }
+
         private void cancelButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void addSupplyButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(serialNumberTextBox.Text))
+            {
+                MessageBox.Show("Необходимо заполнить серийный номер поставки.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (supplierComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Необходимо выбрать поставщика.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (itemsGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("Необходимо добавить хотя бы одну позицию.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // ...
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void productsListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // ...
         }
     }
 }
