692d6ea94dc34812e181aba77ea557d7d0f5e6cf
idekferewf <skadeshopp@gmail.com>
2025-11-25 19:03:10 +0300

    добавлен класс SupplyReportGenerator для генерации отчёта по поставке && добавлены пустные методы GenerateSupplyReport и SaveReportToFile

---
+++ AptekaEu/AptekaEuLib/supplies/SupplyReportGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace AptekaEuLib.supplies
+{
+    public class SupplyReportGenerator
+    {
+        public string GenerateSupplyReport(Supply supply)
+        {
+            return string.Empty;
+        }
+
+        public void SaveReportToFile(Supply supply, string filePath)
+        {
+        }
+    }
+}
